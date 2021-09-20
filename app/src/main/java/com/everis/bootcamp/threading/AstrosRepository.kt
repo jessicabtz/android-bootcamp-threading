0a277580b85876d15b4465fe40b9664bc92dc6a0
Jessica Beatriz <jessicabeatriz@id.uff.br>
2021-09-20 12:28:27 -0300

    implementando AstrosRepository

--- app/src/main/java/com/everis/bootcamp/threading/AstrosRepository.kt
+++ app/src/main/java/com/everis/bootcamp/threading/AstrosRepository.kt
@@ -1,10 +1,22 @@
 package com.everis.bootcamp.threading
 
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
-//TODO: 010 - Criar a classe responsável por carregar os dados
+
 class AstrosRepository {
 
-    //TODO: 011 - Criar função para carregar os astronautas
+    fun loadData(): List<AstroPeople>{
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("http://api.open-notify.org/astros.json")
+            .build()
+        val response = client.newCall(request).execute()
+        val result = parseJsonToResult(response.body?.string())
 
-    //TODO: 012 - Criar função para converter o json
+        return result.people
+    }
+
+    fun parseJsonToResult(json : String?) = Gson().fromJson(json, AstroResult:: class.java)
 }
